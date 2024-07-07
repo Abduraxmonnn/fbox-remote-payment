2bc93a39a82e6524bd610fc46814fa3f56c8a456
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-07-07 19:33:51 +0500

    Added constants

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import payme_icon from "../assets/images/payme-icon.svg";
+import click_icon from "../assets/images/click-icon.svg";
+import uzum_icon from "../assets/images/uzum-icon.svg";
+import anor_icon from "../assets/images/anor-icon.svg";
+
+export const images = {
+    payme_icon,
+    click_icon,
+    uzum_icon,
+    anor_icon
+}
