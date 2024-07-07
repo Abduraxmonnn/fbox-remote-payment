740deb2f35f8d2747324df6ecc0cfff0c6224ef8
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-07-07 19:34:29 +0500

    Added added icons to buttons and animations

--- src/components/PaymentSerivces/PaymentServices.jsx
+++ src/components/PaymentSerivces/PaymentServices.jsx
@@ -1,30 +1,41 @@
-import './PaymentServices.scss'
+import {useEffect} from "react";
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import {images} from '../../constants';
+import './PaymentServices.scss';
 
 const PaymentServices = () => {
 
+    useEffect(() => {
+        AOS.init({duration: 2000});
+    }, [])
+
     return (
         <section className='service-main'>
-            <h1>Добро пожаловать!</h1>
+            <h1 data-aos="zoom-in">Добро пожаловать!</h1>
+            <h3 data-aos="zoom-in">Пожалуйста, выберите один из способов оплаты</h3>
             <div className="service-content">
                 <ul>
-                    <li>
+                    <li data-aos="zoom-in-up">
                         <button className='service-button'>
-                            PayMe
+                            <img src={images.payme_icon} alt="PayMe"/>
                         </button>
                     </li>
-                    <li>
+                    <li data-aos="zoom-in-up">
                         <button className='service-button'>
-                            Click
+                            <img src={images.click_icon} alt="Click"/>
                         </button>
                     </li>
-                    <li>
+                    <li data-aos="zoom-in-up">
                         <button className='service-button'>
-                            Uzum
+                            <img src={images.uzum_icon} alt="Uzum"/>
                         </button>
                     </li>
-                    <li>
+                    <li data-aos="zoom-in-up">
                         <button className='service-button'>
-                            Anor
+                            <img src={images.anor_icon} alt="Anor"/>
                         </button>
                     </li>
                 </ul>
