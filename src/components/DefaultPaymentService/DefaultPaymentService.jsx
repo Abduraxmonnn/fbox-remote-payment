60ee0b5ea2131163779bb9956c2821ecdfb09a3a
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-06-09 14:54:32 +0500

    modified(design):

--- src/components/DefaultPaymentService/DefaultPaymentService.jsx
+++ src/components/DefaultPaymentService/DefaultPaymentService.jsx
@@ -1,6 +1,5 @@
 import Modal from 'react-modal'
 import 'aos/dist/aos.css'
-import { ChevronRight } from 'lucide-react'
 import '../PaymentServices.scss'
 import { images } from '../../constants'
 
@@ -15,89 +14,45 @@ const paymentMethods = [
 
 export default function DefaultPaymentServices() {
 	return (
-		<div className='payment-page default-page'>
-			<filter id='blur'>
-				<feGaussianBlur stdDeviation='3.5' edgeMode='duplicate' />
-			</filter>
-			<div className='banner'>
-				<img
-					src={images.default_banner}
-					alt='Restaurant banner'
-					className='banner-image'
-				/>
-				<div className='banner-overlay'>
-					<div className='logo-container'>
-						<div className='logo'>
-							<img
-								src={images.default_store}
-								alt='store-image'
-								className='store-image'
-							/>
-						</div>
-						<div>
-							<h1 className='restaurant-name'>
-								Scan 2 Pay
-							</h1>
-							<p className='restaurant-address'>
-								<img
-									src={images.locationIcon}
-									alt='locaiton icon'
-									className='location-image'
-								/>
-								Uzbekistan, Tashkent
-							</p>
+		<div className="payment-page default-page">
+			<div className="banner">
+				<img src={images.default_banner || "/placeholder.svg"} alt="Restaurant banner" className="banner-image" />
+				<div className="banner-overlay">
+					<div className="logo-container">
+						<div className="logo">
+							<img src={images.default_store || "/placeholder.svg"} alt="store-image" className="store-image" />
 						</div>
 					</div>
 				</div>
 			</div>
 
-			<div className='content'>
-				<h2 className='section-title'>
-					Итого к оплате
-				</h2>
-				<p className='total-amount'>
-					{(0).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ')}
-				</p>
-				<p className='invoice-number'>
-					Счет №46454
-				</p>
+			<div className="content">
+				<div className="address-container">
+					<p className="restaurant-address">улица Тараса Шевченко, 28</p>
+				</div>
 
-				<h3 className='section-title'>
-					Выберите способы оплаты
-				</h3>
+				<div className="amount-container">
+					<p className="total-amount">
+						141 800 <span className="currency">сум</span>
+					</p>
+				</div>
 
-				<div className='button-section'>
-					<div className='divider'></div>
+				<p className="invoice-number">Счет №446454</p>
 
-					{paymentMethods.map(method => (
-						<div
-							key={method.name}
-							className='payment-method'
-						>
-							<div className='method-info'>
-								<div className='method-icon'>
-									<img
-										src={method.icon}
-										alt={`${method.name} icon`}
-										className='icon-image'
-									/>
-								</div>
-								<span className='payment-provider'>
-									{method.name}
-									{method.isWork && (
-										<span className='soon-payment-provider'>
-											{' '}
-											- {method.isWork}
-										</span>
-									)}
-								</span>
+				<h3 className="section-title">Выберите способ оплаты</h3>
+
+				<div className="payment-methods-grid">
+					{paymentMethods.map((method) => (
+						<div key={method.name} className="payment-method-card">
+							<div className="method-icon">
+								<img src={method.icon || "/placeholder.svg"} alt={`${method.name} icon`} className="icon-image" />
 							</div>
-							<ChevronRight className='chevron' />
+							<span className="payment-provider">{method.name}</span>
 						</div>
 					))}
-
-					<p className='footer'>Design powered by FiscalBox</p>
 				</div>
+
+				<p className="footer">scan2pay powered by FiscalBox</p>
 			</div>
 		</div>
 	)
